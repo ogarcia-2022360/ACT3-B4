bce029dbb690b1f405e5f44f63069ae9bcf27223
ogarcia-2022360 <ogarcia-2022360@kinal.edu.gt>
2026-09-06 23:17:33 -0600

    feat: agregar componentes

---
+++ components/producto-lista/producto-lista.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Producto } from '../../models/producto.model';
+import { CarritoService } from '../../services/carrito.service';
+
+@Component({
+  selector: 'app-producto-lista',
+  templateUrl: './producto-lista.component.html',
+  styleUrls: ['./producto-lista.component.css']
+})
+export class ProductoListaComponent {
+  productos: Producto[] = [
+    { id: 1, nombre: 'Producto 1', precio: 25.00, imagenUrl: 'assets/img/prod1.jpg' },
+    { id: 2, nombre: 'Producto 2', precio: 40.50, imagenUrl: 'assets/img/prod2.jpg' },
+    { id: 3, nombre: 'Producto 3', precio: 15.99, imagenUrl: 'assets/img/prod3.jpg' }
+  ];
+
+  constructor(private carritoService: CarritoService) {}
+
+  agregarAlCarrito(producto: Producto): void {
+    this.carritoService.agregarProducto(producto);
+  }
+}
